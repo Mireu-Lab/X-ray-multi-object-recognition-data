838a6bcf68c06424daabb1fe1f9af601b542e917
mireu-lab <limmireu1214@gmail.com>
2024-07-28 08:22:37 +0900

    update model_download.sh and train.sh

--- model_download.sh
+++ model_download.sh
@@ -1,6 +1,14 @@
 mkdir -p /home/Hosting/workspace/Model/weights
 
+wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-s-converted.pt
+wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-m-converted.pt
+wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-c-converted.pt
+wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-e-converted.pt
+wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-s.pt
+wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-m.pt
 wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-c.pt
 wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-e.pt
+wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/gelan-s.pt
+wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/gelan-m.pt
 wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/gelan-c.pt
 wget -P /home/Hosting/workspace/Model/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/gelan-e.pt
--- train.sh
+++ train.sh
@@ -1,12 +1,9 @@
-# python yolov9/train.py \
-# --batch 16 --epochs 20 --img 640 --device 0 --min-items 0 --close-mosaic 15 \
-# --data /home/Hosting/workspace/X-ray_multi_object-1/data.yaml \
-# --weights /home/Hosting/workspace/Model/weights/gelan-c.pt \
-# --cfg /home/Hosting/workspace/yolov9/models/detect/gelan-c.yaml \
-# --hyp /home/Hosting/workspace/hyp.scratch-high.yaml
+#!/bin/bash
 
-python yolov9/train.py \
---batch 16 --epochs 20 --img 640 --device 0 --min-items 0 --close-mosaic 15 \
---data /home/Hosting/workspace/X-ray_multi_object-1/data.yaml \
---weights /home/Hosting/workspace/Model/weights/gelan-c.pt \
---cfg /home/Hosting/workspace/yolov9/models/detect/gelan-c.yaml
+python3 yolov9/train.py \
+--batch 16 --epochs 20 --img 640 --min-items 0 --close-mosaic 15 \
+--data $(pwd)/X-ray_multi_object-1/data.yaml \
+--weights $(pwd)/Model/weights/gelan-c.pt \
+--cfg $(pwd)/yolov9/models/detect/gelan-c.yaml \
+--hyp $(pwd)/Model/hyp/hyp.scratch-high.yaml \
+--device 0
